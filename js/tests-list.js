c0fa690af364e75cdca7760c009530b0fb2a2db0
Gauravcq <gouravssc77@gmail.com>
2025-11-19 23:44:00 +0530

    new test

--- js/tests-list.js
+++ js/tests-list.js
@@ -218,197 +218,217 @@ const ALL_TESTS = [
         subject: "English",
         isNew: true // ⭐ ADDED: Mark the Reasoning test as new
     },
-    [
   {
-    "id": "ssc_cgl_eng_18_sep_s1",
-    "date": "18 Sep",
-    "title": "Shift 1",
-    "subject": "English",
+    id: "ssc_cgl_eng_17_sep_s1",
+    date: "17 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_17_sep_s2",
+    date: "17 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_17_sep_s3",
+    date: "17 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_18_sep_s1",
+    date: "18 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_18_sep_s2",
+    date: "18 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_18_sep_s3",
+    date: "18 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_19_sep_s1",
+    date: "19 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_19_sep_s2",
+    date: "19 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_19_sep_s3",
+    date: "19 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_20_sep_s1",
+    date: "20 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_20_sep_s2",
+    date: "20 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_20_sep_s3",
+    date: "20 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_21_sep_s1",
+    date: "21 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_21_sep_s2",
+    date: "21 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_eng_21_sep_s3",
+    date: "21 Sep",
+    title: "Shift 3",
+    subject: "English",
     "isNew": true
   },
   {
-    "id": "ssc_cgl_eng_18_sep_s2",
-    "date": "18 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_22_sep_s1",
+    date: "22 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_18_sep_s3",
-    "date": "18 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_22_sep_s2",
+    date: "22 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_19_sep_s1",
-    "date": "19 Sep",
-    "title": "Shift 1",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_22_sep_s3",
+    date: "22 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_19_sep_s2",
-    "date": "19 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_23_sep_s1",
+    date: "23 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_19_sep_s3",
-    "date": "19 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_23_sep_s2",
+    date: "23 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_20_sep_s1",
-    "date": "20 Sep",
-    "title": "Shift 1",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_23_sep_s3",
+    date: "23 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_20_sep_s2",
-    "date": "20 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_24_sep_s1",
+    date: "24 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_20_sep_s3",
-    "date": "20 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_24_sep_s2",
+    date: "24 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_21_sep_s1",
-    "date": "21 Sep",
-    "title": "Shift 1",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_24_sep_s3",
+    date: "24 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_21_sep_s2",
-    "date": "21 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_25_sep_s1",
+    date: "25 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_21_sep_s3",
-    "date": "21 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_25_sep_s2",
+    date: "25 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_22_sep_s1",
-    "date": "22 Sep",
-    "title": "Shift 1",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_25_sep_s3",
+    date: "25 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_22_sep_s2",
-    "date": "22 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_26_sep_s1",
+    date: "26 Sep",
+    title: "Shift 1",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_22_sep_s3",
-    "date": "22 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_26_sep_s2",
+    date: "26 Sep",
+    title: "Shift 2",
+    subject: "English",
+    isNew: true
   },
   {
-    "id": "ssc_cgl_eng_23_sep_s1",
-    "date": "23 Sep",
-    "title": "Shift 1",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_23_sep_s2",
-    "date": "23 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_23_sep_s3",
-    "date": "23 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_24_sep_s1",
-    "date": "24 Sep",
-    "title": "Shift 1",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_24_sep_s2",
-    "date": "24 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_24_sep_s3",
-    "date": "24 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_25_sep_s1",
-    "date": "25 Sep",
-    "title": "Shift 1",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_25_sep_s2",
-    "date": "25 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_25_sep_s3",
-    "date": "25 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_26_sep_s1",
-    "date": "26 Sep",
-    "title": "Shift 1",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_26_sep_s2",
-    "date": "26 Sep",
-    "title": "Shift 2",
-    "subject": "English",
-    "isNew": true
-  },
-  {
-    "id": "ssc_cgl_eng_26_sep_s3",
-    "date": "26 Sep",
-    "title": "Shift 3",
-    "subject": "English",
-    "isNew": true
+    id: "ssc_cgl_eng_26_sep_s3",
+    date: "26 Sep",
+    title: "Shift 3",
+    subject: "English",
+    isNew: true
   }
-]
+
 
     
 
