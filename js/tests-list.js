ee71a5ff87941ed22f2e688b23ffa9bd5b572c60
Gauravcq <gouravssc77@gmail.com>
2025-11-19 23:47:36 +0530

    new test

--- js/tests-list.js
+++ js/tests-list.js
@@ -67,6 +67,279 @@ const ALL_TESTS = [
         subject: "Maths",
         isNew: true // ⭐ ADDED: Mark this test as new
     },
+  {
+    id: "ssc_cgl_maths_15_sep_s1",
+    date: "15 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_15_sep_s2",
+    date: "15 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_15_sep_s3",
+    date: "15 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_16_sep_s1",
+    date: "16 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_16_sep_s2",
+    date: "16 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_16_sep_s3",
+    date: "16 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_17_sep_s1",
+    date: "17 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_17_sep_s2",
+    date: "17 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_17_sep_s3",
+    date: "17 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_18_sep_s1",
+    date: "18 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_18_sep_s2",
+    date: "18 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_18_sep_s3",
+    date: "18 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_19_sep_s1",
+    date: "19 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_19_sep_s2",
+    date: "19 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_19_sep_s3",
+    date: "19 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_20_sep_s1",
+    date: "20 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_20_sep_s2",
+    date: "20 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_20_sep_s3",
+    date: "20 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_21_sep_s1",
+    date: "21 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_21_sep_s2",
+    date: "21 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_21_sep_s3",
+    date: "21 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_22_sep_s1",
+    date: "22 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_22_sep_s2",
+    date: "22 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_22_sep_s3",
+    date: "22 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_23_sep_s1",
+    date: "23 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_23_sep_s2",
+    date: "23 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_23_sep_s3",
+    date: "23 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_24_sep_s1",
+    date: "24 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_24_sep_s2",
+    date: "24 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_24_sep_s3",
+    date: "24 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_25_sep_s1",
+    date: "25 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_25_sep_s2",
+    date: "25 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_25_sep_s3",
+    date: "25 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_26_sep_s1",
+    date: "26 Sep",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_26_sep_s2",
+    date: "26 Sep",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_26_sep_s3",
+    date: "26 Sep",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_14_oct_s1",
+    date: "14 Oct",
+    title: "Shift 1",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_14_oct_s2",
+    date: "14 Oct",
+    title: "Shift 2",
+    subject: "Maths",
+    isNew: true
+  },
+  {
+    id: "ssc_cgl_maths_14_oct_s3",
+    date: "14 Oct",
+    title: "Shift 3",
+    subject: "Maths",
+    isNew: true
+  },
 
 
     // --- Reasoning Tests (NEWLY ADDED) ---
